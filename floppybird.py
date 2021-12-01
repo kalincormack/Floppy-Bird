e940f5a848697a46f32c63de4ce05076d4edf62c
Kalin Cormack <kalincormack@MacBook-Pro.local>
2021-12-01 14:59:59 -0500

    added different bird positions and text

--- floppybird.py
+++ floppybird.py
@@ -18,7 +18,6 @@ def create_pipe():
     top_pipe = pipe_surface.get_rect(midbottom = (700,random_pipe_pos - 150))
     return bottom_pipe, top_pipe
 
-# 
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 5
@@ -33,7 +32,6 @@ def draw_pipes(pipes):
             flip_pipe = pygame.transform.flip(pipe_surface,False,True)
             screen.blit(flip_pipe,pipe)
 
-
 def check_collision(pipes):
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
@@ -44,14 +42,31 @@ def check_collision(pipes):
 
     return True
 
+def rotate_bird(bird):
+    new_bird = pygame.transform.rotozoom(bird,-bird_movement * 2,1)
+    return new_bird
+
+def bird_animation():
+    new_bird = bird_frames[bird_index]
+    new_bird_rect = new_bird.get_rect(center = (50,bird_rect.centery))
+    return new_bird,new_bird_rect
+
+def score_display():
+    score_surface = game_font.render('Score',True,(255,255,255))
+    score_rect = score_surface.get_rect(center = (144,50))
+    screen.blit(score_surface,score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((288,512))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('04B_19.ttf',20)
 
 # Game Variable
 gravity = 0.25
 bird_movement = 0
 game_active = True
+score = 0
+high_score = 0
 
 # Uploads background iamge
 bg_surface = pygame.image.load('assets/background-day.png').convert()
@@ -60,9 +75,20 @@ bg_surface = pygame.image.load('assets/background-day.png').convert()
 floor_surface = pygame.image.load('assets/base.png').convert()
 floor_x_pos = 0 
 
-# Uploads bird image
-bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
+bird_downflap = pygame.image.load('assets/bluebird-downflap.png').convert_alpha()
+bird_midflap = pygame.image.load('assets/bluebird-midflap.png').convert_alpha()
+bird_upflap = pygame.image.load('assets/bluebird-upflap.png').convert_alpha()
+bird_frames = [bird_downflap,bird_midflap,bird_upflap]
+bird_index = 0
+bird_surface = bird_frames[bird_index]
 bird_rect = bird_surface.get_rect(center = (50,256))
+
+BIRDFLAP = pygame.USEREVENT + 1
+pygame.time.set_timer(BIRDFLAP,200)
+
+# # Uploads bird image
+# bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert_alpha()
+# bird_rect = bird_surface.get_rect(center = (50,256))
 
 # Uploads pipe image and respwans pipes 
 pipe_surface = pygame.image.load('assets/pipe-green.png')
@@ -84,22 +110,33 @@ while True:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (50,256)
+                bird_movement = 0
 
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
+
+        if event.type == BIRDFLAP:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+            
+            bird_surface,bird_rect = bird_animation()
 
     screen.blit(bg_surface,(0,0))
 
     if game_active:
         # Bird
         bird_movement += gravity
+        rotated_bird = rotate_bird(bird_surface)
         bird_rect.centery += bird_movement
-        screen.blit(bird_surface,bird_rect)
+        screen.blit(rotated_bird,bird_rect)
         game_active = check_collision(pipe_list)
 
         # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
+        score_display()
             
     # Floor
     floor_x_pos -= 1
