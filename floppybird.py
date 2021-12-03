68cd4d30c18e1e7cc6aa19c56c2cf975e13517d0
Kalin Cormack <kalincormack@MacBook-Pro.local>
2021-12-03 14:34:37 -0500

    added sound

--- floppybird.py
+++ floppybird.py
@@ -35,6 +35,7 @@ def draw_pipes(pipes):
 def check_collision(pipes):
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
+            death_sound.play()
             return False
 
         if bird_rect.top <= -50 or bird_rect.bottom >= 450:
@@ -51,15 +52,30 @@ def bird_animation():
     new_bird_rect = new_bird.get_rect(center = (50,bird_rect.centery))
     return new_bird,new_bird_rect
 
-def score_display():
-    score_surface = game_font.render('Score',True,(255,255,255))
-    score_rect = score_surface.get_rect(center = (144,50))
-    screen.blit(score_surface,score_rect)
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)),True,(255,255,255))
+        score_rect = score_surface.get_rect(center = (144,50))
+        screen.blit(score_surface,score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score:{int(score)}' ,True,(255,255,255))
+        score_rect = score_surface.get_rect(center = (144,50))
+        screen.blit(score_surface,score_rect)
+        
+        high_score_surface = game_font.render(f'High score:{int(high_score)}',True,(255,255,255))
+        high_score_rect = high_score_surface.get_rect(center = (144,425))
+        screen.blit(high_score_surface,high_score_rect)
 
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
 pygame.init()
 screen = pygame.display.set_mode((288,512))
 clock = pygame.time.Clock()
-game_font = pygame.font.Font('04B_19.ttf',20)
+game_font = pygame.font.Font('./assets/fonts/04B_19.ttf',20)
 
 # Game Variable
 gravity = 0.25
@@ -97,6 +113,14 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE,1200)
 pipe_height = [200,300,400]
 
+game_over_surface = pygame.image.load('assets/message.png').convert_alpha()
+game_over_rect = game_over_surface.get_rect(center = (144,256))
+
+flap_sound = pygame.mixer.Sound('sound/sound_sfx_wing.wav')
+death_sound = pygame.mixer.Sound('sound/sound_sfx_hit.wav')
+score_sound = pygame.mixer.Sound('sound/sound_sfx_point.wav')
+score_sound_countdown = 100
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -106,11 +130,13 @@ while True:
             if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 12  
+                flap_sound.play()
             if event.key == pygame.K_SPACE and game_active == False:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (50,256)
                 bird_movement = 0
+                score = 0
 
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
@@ -136,7 +162,17 @@ while True:
         # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
-        score_display()
+
+        score += 0.01
+        score_display('main_game')
+        score_sound_countdown -= 1
+        if score_sound_countdown <= 0:
+            score_sound.play()
+            score_sound_countdown = 100
+    else:
+        screen.blit(game_over_surface, game_over_rect)
+        high_score = update_score(score, high_score)
+        score_display('game_over')
             
     # Floor
     floor_x_pos -= 1
